bd88d908e0774b66cca30a1a412c57ac19832d17
gh0stgir1 <145555035+gh0stgir1@users.noreply.github.com>
2024-02-01 10:09:36 +0100

    Update Exercici3.sh

--- Exercici3.sh
+++ Exercici3.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
 
+# Comprobar que se proporcionan tres parámetros
+if [ "$#" -ne 3 ]; then
+    echo "Numero incorrecte de paràmetres. Siusplau, proporciona tres paràmetres"
+    exit 1
+fi
+
+# Asignar els parametres a variables
+parametro1=$1
+parametro2=$2
+parametro3=$3
+
+# Mostrar la linea de sortida
+echo "Nom de l’script: ./exercici3.sh"
+echo "Valor del primer paràmetre de l’script: $parametro1"
+echo "Valor del segon paràmetre de l’script: $parametro2"
+echo "Valor del tercer paràmetre de l’script: $parametro3"
