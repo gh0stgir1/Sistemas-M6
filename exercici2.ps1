6448e7d2bb4fcb6b40460c10f0615bc052d72499
gh0stgir1 <145555035+gh0stgir1@users.noreply.github.com>
2024-02-14 09:30:21 +0100

    Update exercici2.ps1

--- exercici2.ps1
+++ exercici2.ps1
@@ -1,1 +1,13 @@
+# Obtener la ruta del escritorio del usuario actual
+$rutaEscritori = [System.Environment]::GetFolderPath('Desktop')
+
+# Crear el directorio "scripts" en el escritorio
+$nouDirectori = Join-Path -Path $rutaEscritori -ChildPath "scripts"
+New-Item  -Path "$\Users\Administrador\Documents" -ItemType Directory
+
+# Agregar el directorio "scripts" al PATH del sistema
+$Env:PATH += ";$nouDirectori"
+
+# Mostrar el nuevo valor de PATH
+Write-Host "El nuevo valor de PATH es: $Env:PATH"
 
