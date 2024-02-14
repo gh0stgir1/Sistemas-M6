46f9f86e386f8e7a0fc0d85b2e68e0bb7c3b1ffe
gh0stgir1 <145555035+gh0stgir1@users.noreply.github.com>
2024-02-14 09:53:32 +0100

    Create exercici4.ps1

---
+++ exercici4.ps1
@@ -0,0 +1,27 @@
+param(
+    [string]$ruta  # Parámetro que recibe la ruta a verificar
+)
+
+# Verificar si $ruta no está vacío
+if ([string]::IsNullOrEmpty($ruta)) {
+    Write-Host "La ruta no puede estar vacía."
+}
+else {
+    # Verificar si la ruta existe
+    if (Test-Path $ruta) {
+        # Verificar si la ruta es un directorio
+        if (Test-Path -Path $ruta -PathType Container) {
+            Write-Host "La ruta '$ruta' es un directorio."
+        }
+        # Verificar si la ruta es un archivo regular
+        elseif (Test-Path -Path $ruta -PathType Leaf) {
+            Write-Host "La ruta '$ruta' es un archivo regular."
+        }
+        else {
+            Write-Host "La ruta '$ruta' no es ni un directorio ni un archivo regular."
+        }
+    }
+    else {
+        Write-Host "La ruta '$ruta' no existe."
+    }
+}
