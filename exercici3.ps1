c2beacdbe32e7eb1ebecb5bd40de1f2b09c6d161
gh0stgir1 <145555035+gh0stgir1@users.noreply.github.com>
2024-02-14 09:39:40 +0100

    Create exercici3.ps1

---
+++ exercici3.ps1
@@ -0,0 +1,9 @@
+param(
+    [string]$primerParametre,
+    [string]$segonParametre,
+    [string]$tercerParametre
+)
+
+Write-Host "Valor del primer paràmetre de l’script: $primerParametre"
+Write-Host "Valor del segon paràmetre de l’script: $segonParametre"
+Write-Host "Valor del tercer paràmetre de l’script: $tercerParametre"
