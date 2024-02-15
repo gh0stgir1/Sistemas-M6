352e1223071c49b21e195ec31fbbaecd5a81ce87
gh0stgir1 <145555035+gh0stgir1@users.noreply.github.com>
2024-02-15 08:34:11 +0100

    Update exercici7.ps1

--- exercici7.ps1
+++ exercici7.ps1
@@ -1,24 +1,17 @@
-#!/bin/bash
-
 # Solicitar al usuario el valor de la nota
-read -p "Introdueix el valor de la nota (un nombre enter): " nota
+$nota = Read-Host "Introdueix el valor de la nota (un nombre enter)"
 
 # Verificar si la entrada es un número entero
-if [[ $nota =~ ^[0-9]+$ ]]; then
-    # Determinar la calificación correspondiente utilizando expresiones aritméticas
-    if (( nota >= 0 && nota <= 2 )); then
-        echo "La nota és una D"
-    elif (( nota >= 3 && nota <= 4 )); then
-        echo "La nota és una C-"
-    elif (( nota >= 5 && nota <= 6 )); then
-        echo "La nota és una C+"
-    elif (( nota >= 7 && nota <= 8 )); then
-        echo "La nota és una B"
-    elif (( nota == 9 || nota == 10 )); then
-        echo "La nota és una A"
-    else
-        echo "La nota introduïda no està dins de l'escala vàlida (0-10)"
-    fi
-else
-    echo "El valor introduït no és un nombre enter vàlid."
-fi
+if ($nota -as [int]) {
+    # Determinar la calificación correspondiente utilizando la estructura switch
+    switch ($nota) {
+        {$_ -in 0..2} { Write-Output "La nota és una D" }
+        {$_ -in 3..4} { Write-Output "La nota és una C-" }
+        {$_ -in 5..6} { Write-Output "La nota és una C+" }
+        {$_ -in 7..8} { Write-Output "La nota és una B" }
+        {$_ -in 9, 10} { Write-Output "La nota és una A" }
+        default { Write-Output "La nota introduïda no està dins de l'escala vàlida (0-10)" }
+    }
+} else {
+    Write-Output "El valor introduït no és un nombre enter vàlid."
+}
